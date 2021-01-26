de2591b7c0ce280aea33e362ee5c06625ae8cbbb
Fernando Ghinzelli <fghinzelli@gmail.com>
2021-01-26 19:59:06 -0300

    Remoção de código antigo

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,6 @@ import QuizBackground from '../src/components/QuizBackground';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
-
 const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
